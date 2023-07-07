88641845751fc63a310b94e77837522656ff46bd
Tim Webster <tim.webster@contino.io>
2023-07-07 15:25:04 +0100

    fix ws

--- main.tf
+++ main.tf
@@ -7,6 +7,6 @@ resource "azurerm_log_analytics_workspace" "containerapp_ws" {
   name                = "containerapp-ws"
   location            = azurerm_resource_group.containerapp_rg.location
   resource_group_name = azurerm_resource_group.containerapp_rg.name
-  sku                 = "Free"
-  retention_in_days   = 7
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
 }
