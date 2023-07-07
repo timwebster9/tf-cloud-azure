9e490121575394005ff8ac3c9214ec38bff4002a
Tim Webster <tim.webster@contino.io>
2023-07-07 15:30:07 +0100

    add container app env

--- main.tf
+++ main.tf
@@ -10,3 +10,10 @@ resource "azurerm_log_analytics_workspace" "containerapp_ws" {
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
+
+resource "azurerm_container_app_environment" "containerapp" {
+  name                       = "capp-01"
+  location                   = azurerm_resource_group.containerapp_rg.location
+  resource_group_name        = azurerm_resource_group.containerapp_rg.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.containerapp_ws.id
+}
