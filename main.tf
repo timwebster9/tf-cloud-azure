6e9642b0e759241aaea1799ad91f3c67ccf86482
Tim Webster <tim.webster@contino.io>
2023-07-07 15:23:39 +0100

    add workspace

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "containerapp_rg" {
   name     = "containerapp-rg"
   location = var.location
 }
+
+resource "azurerm_log_analytics_workspace" "containerapp_ws" {
+  name                = "containerapp-ws"
+  location            = azurerm_resource_group.containerapp_rg.location
+  resource_group_name = azurerm_resource_group.containerapp_rg.name
+  sku                 = "Free"
+  retention_in_days   = 7
+}
