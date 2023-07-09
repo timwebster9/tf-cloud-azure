7399a20aad0f7ff16ae02bc4cd5cfb815be21226
Tim Webster <tim.webster@contino.io>
2023-07-09 16:45:53 +0100

    update

--- postgres.tf
+++ postgres.tf
@@ -10,6 +10,9 @@ resource "azurerm_postgresql_flexible_server" "postgres" {
   storage_mb             = 32768
   sku_name               = "B_Standard_B1ms"
   zone                   = "1"
+
+  // Errors if the DNS zone VNet link isn't in place at the time of server deployment
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.postgres_zone_link]
 }
 
 resource "azurerm_postgresql_flexible_server_configuration" "postgres_config" {
